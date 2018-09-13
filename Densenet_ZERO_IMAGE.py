be5b7ee1fba1133cbb9e6db9923fb42dbe1696fc
halostormls <2524416894@qq.com>
2018-09-13 18:40:32 +0800

    modify

--- Densenet_ZERO_IMAGE.py
+++ Densenet_ZERO_IMAGE.py
@@ -209,9 +209,9 @@ class DenseNet():
 
 
 def train1():
-    image = tf.placeholder(shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH], dtype=tf.float32,
+    image = tf.placeholder(shape=[batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH], dtype=tf.float32,
                            name='image_placeholder')
-    label = tf.placeholder(shape=[None, LABEL_SIZE], dtype=tf.float32, name='label_palceholder')
+    label = tf.placeholder(shape=[batch_size, LABEL_SIZE], dtype=tf.float32, name='label_palceholder')
 
     train_flag = tf.placeholder(dtype=tf.bool, name='flag_placeholder')
 
@@ -265,95 +265,6 @@ def train1():
         print('iter: %i, loss: %f' % (i, loss_data))
 
     saver.save(sess=sess, save_path='./model/dense.ckpt')
-
-
-def train(images, labels):
-    x = tf.placeholder(tf.float32, shape=[None, 12288])
-
-    image = tf.placeholder(shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH], dtype=tf.float32,
-                           name='image_placeholder')
-    label = tf.placeholder(shape=[None, LABEL_SIZE], dtype=tf.float32, name='label_palceholder')
-
-    batch_x, batch_y = tf.train.shuffle_batch([img, label], batch_size,
-                                              capacity=10 + 100 * batch_size,
-                                              min_after_dequeue=10)
-
-    training_flag = tf.placeholder(tf.bool)
-
-    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-
-    logits = DenseNet(x=image, nb_blocks=nb_block, filters=growth_k, training=training_flag).model
-
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logits))
-
-    # l2_loss = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
-    # optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=nesterov_momentum, use_nesterov=True)
-    # train = optimizer.minimize(cost + l2_loss * weight_decay)
-
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=epsilon)
-    train = optimizer.minimize(cost)
-
-    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(label, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    tf.summary.scalar('loss', cost)
-    tf.summary.scalar('accuracy', accuracy)
-
-    saver = tf.train.Saver(tf.global_variables())
-
-    with tf.Session() as sess:
-        ckpt = tf.train.get_checkpoint_state('./model')
-        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
-            saver.restore(sess, ckpt.model_checkpoint_path)
-        else:
-            sess.run(tf.global_variables_initializer())
-
-        merged = tf.summary.merge_all()
-        writer = tf.summary.FileWriter('./logs', sess.graph)
-
-        global_step = 0
-        epoch_learning_rate = init_learning_rate
-        for epoch in range(total_epochs):
-            if epoch == (total_epochs * 0.5) or epoch == (total_epochs * 0.75):
-                epoch_learning_rate = epoch_learning_rate / 10
-
-            total_batch = int(38220 / batch_size)
-
-            for step in range(total_batch):
-                # 使用shuffle_batch可以随机打乱输入
-                print(batch_x)
-                print(batch_y)
-
-                train_feed_dict = {
-                    x: batch_x,
-                    label: batch_y,
-                    learning_rate: epoch_learning_rate,
-                    training_flag: True
-                }
-
-                _, loss = sess.run([train, cost], feed_dict=train_feed_dict)
-
-                if step % 50 == 0:
-                    global_step += 50
-                    train_summary, train_accuracy = sess.run([merged, accuracy], feed_dict=train_feed_dict)
-                    # accuracy.eval(feed_dict=feed_dict)
-                    print("Step:", step, "Loss:", loss, "Training accuracy:", train_accuracy)
-                    writer.add_summary(train_summary, global_step=epoch)
-
-                    #     test_feed_dict = {
-                    #         x: mnist.test.images,
-                    #         label: mnist.test.labels,
-                    #         learning_rate: epoch_learning_rate,
-                    #         training_flag: False
-                    #     }
-                    #
-                    # accuracy_rates = sess.run(accuracy, feed_dict=test_feed_dict)
-                    # print('Epoch:', '%04d' % (epoch + 1), '/ Accuracy =', accuracy_rates)
-                    # # writer.add_summary(test_summary, global_step=epoch)
-
-        saver.save(sess=sess, save_path='./model/dense.ckpt')
-
 
 if __name__ == '__main__':
     print()

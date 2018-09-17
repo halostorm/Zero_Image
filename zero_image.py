5d8dd32d5622d7b63dc6c1b0b00e29760ff7673e
halostormls <2524416894@qq.com>
2018-09-17 16:10:19 +0800

    m

--- zero_image.py
+++ zero_image.py
@@ -62,8 +62,8 @@ def load_data(dir, path):
         image = img_to_array(image)
         # extract the class label from the image path and update the
         # labels list
-        label = int(file[1])
-        # label = np.array(label)
+        label = file[1]
+        label = np.array(label)
         if count < 30000:
             data.append(image)
             labels.append(label)
@@ -120,6 +120,9 @@ def train():
     trainX = trainX.astype('float32')
     testX = testX.astype('float32')
 
+    Y_train = np_utils.to_categorical(trainY, nb_classes)
+    Y_test = np_utils.to_categorical(testY, nb_classes)
+
     generator = ImageDataGenerator(rotation_range=15,
                                    width_shift_range=5. / img_rows,
                                    height_shift_range=5. / img_cols,
@@ -142,12 +145,10 @@ def train():
 
     callbacks = [lr_reducer, model_checkpoint]
 
-    print(testX.shape[0])
-
-    history = model.fit_generator(generator.flow(trainX, trainY, batch_size=batch_size),
+    history = model.fit_generator(generator.flow(trainX, Y_train, batch_size=batch_size),
                         steps_per_epoch=len(trainX) // batch_size, epochs=nb_epoch,
                         callbacks=callbacks,
-                        validation_data=(testX, testY),
+                        validation_data=(testX, Y_test),
                         validation_steps=testX.shape[0] // batch_size, verbose=1)
 
     # save
